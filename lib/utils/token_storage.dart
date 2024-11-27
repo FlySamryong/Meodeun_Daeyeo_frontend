35cb51556551743b24549acbb4ceaf5865f200a3
jeongchanmin <jjw421412@naver.com>
2024-11-28 03:48:04 +0900

    :bug: [Bug] 디버깅

--- lib/utils/token_storage.dart
+++ lib/utils/token_storage.dart
@@ -1,38 +1,44 @@
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'dart:html' as html;
 
 class TokenStorage {
-  static const _storage = FlutterSecureStorage();
+  static html.Storage _storage = html.window.localStorage;
 
   /// 사용자 ID를 저장하는 함수
   static Future<void> saveUserId(int userId) async {
-    await _storage.write(key: 'userId', value: userId.toString());
+    _storage['userId'] = userId.toString();
   }
 
   /// 액세스 토큰과 리프레시 토큰을 저장하는 함수
-  static Future<void> saveTokens(String accessToken, String refreshToken) async {
-    await _storage.write(key: 'accessToken', value: accessToken);
-    await _storage.write(key: 'refreshToken', value: refreshToken);
+  static Future<void> saveTokens(
+      String accessToken, String refreshToken) async {
+    _storage['accessToken'] = accessToken;
+    _storage['refreshToken'] = refreshToken;
   }
 
   /// 사용자 ID를 가져오는 함수
   static Future<int?> getUserId() async {
-    var userId = await _storage.read(key: 'userId');
+    var userId = _storage['userId'];
     return userId != null ? int.parse(userId) : null;
   }
 
   /// 액세스 토큰을 가져오는 함수
   static Future<String?> getAccessToken() async {
-    return await _storage.read(key: 'accessToken');
+    return _storage['accessToken'];
   }
 
   /// 리프레시 토큰을 가져오는 함수
   static Future<String?> getRefreshToken() async {
-    return await _storage.read(key: 'refreshToken');
+    return _storage['refreshToken'];
   }
 
   /// 저장된 토큰을 모두 삭제하는 함수
   static Future<void> clearTokens() async {
-    await _storage.delete(key: 'accessToken');
-    await _storage.delete(key: 'refreshToken');
+    _storage.remove('accessToken');
+    _storage.remove('refreshToken');
+  }
+
+  /// 사용자 ID를 삭제하는 함수
+  static Future<void> clearUserId() async {
+    _storage.remove('userId');
   }
 }
